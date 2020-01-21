d619edc8bfa81e8f153858f78b25c38e5fc19545
Shintarooo000 <kataoka240407@gmail.com>
2020-01-21 06:46:09 +0000

    aiueo]

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'],function(){
+   Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+   Route::get('/profile/create','Admin\ProfileController@add')->middleware('auth');
+   Route::get('/profile/edit','Admin\ProfileController@edit')->middleware('auth');
+});
+
+//課題3
+Route::get('/XXX','Admin\AAAController@bbb');
+
+//課題4
+Route::get('/profile/create','Admin\ProfileController@add');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function(){
+    Route::get('new/create', 'Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
+    Route::post('profile/create', 'Admin\ProfileController@crate');
+});
